33f3c3929f5fc34dbad5d7dd529b33793d9e6ad2
waawal <waawal@boom.ws>
2012-05-08 13:42:59 +0200

    fixing image

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "http://farm4.staticflickr.com/3043/2343360380_fdbd835cff_t.jpg"
+html_logo = "leaf.jpg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
