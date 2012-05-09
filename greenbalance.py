0d90dcdfe1236c8a30d1bbe399201efa925e03ce
waawal <waawal@boom.ws>
2012-05-09 22:20:29 +0200

    added logging points

--- greenbalance.py
+++ greenbalance.py
@@ -19,13 +19,17 @@ class PortForwarder(StreamServer):
     def __init__(self, listener, destinations, **kwargs):
         StreamServer.__init__(self, listener, **kwargs)
         self.destinations = destinations
+        logging.debug('Starting up the port forwarder.')
 
     def handle(self, source, address):
         target = self.get_destination()
         try:
             destination = create_connection(target)
-        except IOError:
+        except IOError, ex:
             # TODO: Future implementation of health check.
+            logging.error('%s:%s failed to connect to %s:%s: %s' % (address[0],
+                          address[1], self.destination[0], self.destination[1],
+                          ex))
             return
         forwarder = gevent.spawn(self.forward, source, destination)
         backforwarder = gevent.spawn(self.forward, destination, source)
@@ -33,6 +37,7 @@ class PortForwarder(StreamServer):
 
     def close(self):
         if self.closed:
+            logging.critical('Multiple exit signals received - aborting.')
             sys.exit('Multiple exit signals received - aborting.')
         else:
             StreamServer.close(self)
